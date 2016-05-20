0ddc2540479b18a07aaa4e7518083dcd57a332f8
Diogo <Diogo@192.168.0.16>
2016-05-19 21:28:51 -0300

    atualizada title

---
+++ berrine/empresa-motoboy/index.php
@@ -0,0 +1,195 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="keywords" content="motoboy sp, empresa motoboy, Serviço Motoboy, motoboy em sp, motoboy Brooklin">
+  <meta name="description" content="A ExpressPack empresa de Motoboy no Brooklin especializada na entrega de documentos, serviços de entraga rapida, entrega de malote -  Brooklin .">
+  <meta name="author" content="Expresspack Brooklin">
+
+  <title>Expresspack Brooklin - Empresa de motoboy em São Paulo - motoboy sp</title>
+	<meta name="google-site-verification" content="hxNIlB1ckLI1kL9ww_5s2F8I4ClConc5JaZ0AFbAHek" />
+	<meta name="url" content="http://berrine.expresspack.com.br">
+	<meta name="language" content="Portugues">
+	<meta name="Robots" content="index,follow, all" />
+	<meta name="Googlebot" content="index,follow, all" />
+	<meta name="InktomiSlurp" content="index, follow, all" />
+	<meta name="Unknownrobot" content="index, follow, all" />
+	<meta name="MSNBot" content="index, follow, all" />
+	<meta http-equiv="content-Language" content="pt-br" />
+
+    
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <script type="text/javascript">
+
+	  var _gaq = _gaq || [];
+	  _gaq.push(['_setAccount', 'UA-1189029-8']);
+	  _gaq.push(['_setDomainName','expresspack.com.br']);
+	  _gaq.push(['_trackPageview']);
+
+	  (function() {
+	    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+	    ga.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'stats.g.doubleclick.net/dc.js';
+	    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+	  })();
+
+	</script>
+	<script>
+  !function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+  n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
+  n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
+  t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
+  document,'script','https://connect.facebook.net/en_US/fbevents.js');
+
+  fbq('init', '1068366543222116');
+  fbq('track', "PageView");</script>
+  <noscript><img height="1" width="1" style="display:none"
+  src="https://www.facebook.com/tr?id=1068366543222116&ev=PageView&noscript=1"
+  /></noscript>
+  <!-- End Facebook Pixel Code -->
+</head>
+
+<body>
+
+    <?php include'../includes/menu.php';?>
+
+    <!-- Page Content -->
+    <div class="container">
+    	<a href="#0" class="cd-top">Top</a> <!-- cd-nugget-info -->
+        
+    	<h1 class="chamada">Nossa Visão</h1>
+      <p>Nosso mercado se diferencia pela dinâmica e eficiência de cada prestador de serviços, levando ao seus clientes maior rapidez, agilidade e confiança no servi que presta, tornando desta forma nossa prestação de serviços cada vez mais econômica. Estamos sempre buscando novas alternativas para a satisfação de nossos clientes, tornando nossa prestação de serviço o mais eficaz possível.</p>
+        
+    	<h1 class="chamada">Nosso Compromisso</h1>
+      <p>Nosso compromisso é a integral satisfação de nossos clientes e colaboradores, estamos sempre focados em atender as necessidades de nossos clientes nos serviços que prestamos e dando ferramentas modernas e atualizadas a nossos colaboradores, tornando a pró-atividade uma realidade em nosso cotidiano.</p>
+      <p>Nos comprometemos em trabalhar com preços justos e prestar o melhor serviço possível, estando atento as necessidades de todos envolvidos no processo.</p>
+
+      <h1 class="chamada">Nossa Empresa</h1>
+      <p>Somos uma empresa de transporte de pequenas cargas, utilizamos como meio de transportes motocicletas , fiorinos, vans e pequenos caminhões, temos uma grande experiência no ramos de entregas, todos nossos colaboradores, são altamente capacitados, treinados e verdadeiros profissionais no ramo, todos colaboradores são registrados CLT, todos trabalham uniformizados com todo EPI necessário ao trabalho que exerce. Sabemos que nas grandes metrópoles se tornou cada dia mais difícil sua locomoção e transporte do cotidiano, nós cientes desta realidade, estamos sempre buscando atender nosso clientes com a maior eficiência possível, tornando os custos mais acessíveis.</p>
+
+      <h1 class="chamada">Onde Atuamos</h1>
+      <p>Estamos localizados no coração da Cidade de São Paulo, nossa atuação está em toda a São Paulo e grande São Paulo, nos serviços de motoboy ou entrega rápida, não só através de serviços de motoboy , mas, com fiorinos, vans e pequenos caminhões, além desta modalidade, entregamos em do Estado de São Paulo não só documentos com verdadeira urgência, como malotes diários à todas as cidades.</p>
+    </div>
+    <!-- /.container -->
+    <div class="jumbotron jumbotron-sm">
+    	<div class="container">
+	        <div class="row">
+	            <div class="col-sm-12 col-lg-12">
+	                <h1 class="h1">
+	                    Contato <small>Entre em contato com a Expresspack Brooklin</small></h1>
+	            </div>
+	        </div>
+    	</div>
+	</div>
+	<div class="maps" id="contato">
+	<div class="container">
+	    <div class="row">
+	        <div class="col-md-8">
+	            <div class="well well-sm">
+	                <form>
+	                <div class="row">
+	                    <div class="col-md-6">
+	                        <div class="form-group">
+	                            <label for="name">
+	                                Nome</label>
+	                            <input type="text" class="form-control" id="name" placeholder="Seu nome" required="required" />
+	                        </div>
+	                        <div class="form-group">
+	                            <label for="email">
+	                                Endereço de E-mail</label>
+	                            <div class="input-group">
+	                                <span class="input-group-addon"><span class="glyphicon glyphicon-envelope"></span>
+	                                </span>
+	                                <input type="email" class="form-control" id="email" placeholder="Entre com um email" required="required" /></div>
+	                        </div>
+	                        <div class="form-group">
+	                            <label for="subject">
+	                                Assunto</label>
+	                            <select id="subject" name="subject" class="form-control" required="required">
+	                                <option value="na" selected="">Escolha um assunto:</option>
+	                                <option value="service">Financeiro</option>
+	                                <option value="suggestions">S.A.C</option>
+	                                <option value="product">Trabalhe conosco</option>
+	                            </select>
+	                        </div>
+	                    </div>
+	                    <div class="col-md-6">
+	                        <div class="form-group">
+	                            <label for="name">
+	                                Mensagem</label>
+	                            <textarea name="message" id="message" class="form-control" rows="9" cols="25" required="required"
+	                                placeholder="Mensagem"></textarea>
+	                        </div>
+	                    </div>
+	                    <div class="col-md-12">
+	                        <button type="submit" class="btn btn-primary pull-right" id="btnContactUs">
+	                            Enviar Contato</button>
+	                    </div>
+	                </div>
+	                </form>
+	            </div>
+	        </div>
+	        <div class="col-md-4 detalhes">
+	            <form>
+	            <legend><span class="glyphicon glyphicon-globe"></span> Nosso Escritorio</legend>
+	            <address>
+	                <strong>ExpressPack, Brooklin.</strong><br>
+	                Rua Pensilvania, Brooklin, 1429<br>
+	                São Paulo, SP 01311-000<br>
+	                <abbr title="Phone">
+	                    brooklin:</abbr>
+	                (11) 5505-3406 / (11) 5505-3407
+	            </address>
+	            <address>
+	                <strong>Contato Brooklin</strong><br>
+	                <a href="mailto:#">adm@expresspack.com.br</a>
+	            </address>
+	            </form>
+	        </div>
+	    </div>
+	</div>
+	</div>
+
+
+
+	<?php include'../includes/footer.php';?>
+    <!-- child of the body tag -->
+	<script type="text/javascript">
+		$(document).ready(function() {
+		    $('#Carousel').carousel({
+		        interval: 5000
+		    })
+		});
+	</script>
+    <!-- jQuery -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/backtop.js"></script>
+    <script type="text/javascript">
+jQuery(document).ready(function($) { 
+    $(".scroll").click(function(event){        
+        event.preventDefault();
+        $('html,body').animate({scrollTop:$(this.hash).offset().top}, 800);
+   });
+});
+</script>
+</body>
+<!-- Bootstrap Core CSS -->
+    <link href="../css/bootstrap.min.css" rel="stylesheet">
+    <link href="../css/bootstrap-theme.min.css" rel="stylesheet">
+    <!-- Custom CSS -->
+    <link href="../css/style.css" rel="stylesheet">
+    <link rel=”s"../css/style.css" id="font-awesome-css" href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" type="text/css" media="screen">
+    <link href='https://fonts.googleapis.com/css?family=Lobster|Open+Sans' rel='stylesheet' type='text/css'>
+</html>
---
+++ berrine/index.php
@@ -0,0 +1,340 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="keywords" content="motoboy sp, empresa motoboy, Serviço Motoboy, motoboy em sp, motoboy Brooklin">
+  <meta name="description" content="A ExpressPack empresa de Motoboy no Brooklin especializada na entrega de documentos, serviços de entraga rapida, entrega de malote -  Brooklin .">
+  <meta name="author" content="Expresspack Brooklin">
+
+  <title>Expresspack Brooklin - Empresa de motoboy em São Paulo - motoboy sp</title>
+	<meta name="google-site-verification" content="hxNIlB1ckLI1kL9ww_5s2F8I4ClConc5JaZ0AFbAHek" />
+	<meta name="url" content="http://berrine.expresspack.com.br">
+	<meta name="language" content="Portugues">
+	<meta name="Robots" content="index,follow, all" />
+	<meta name="Googlebot" content="index,follow, all" />
+	<meta name="InktomiSlurp" content="index, follow, all" />
+	<meta name="Unknownrobot" content="index, follow, all" />
+	<meta name="MSNBot" content="index, follow, all" />
+	<meta http-equiv="content-Language" content="pt-br" />
+
+    
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <script type="text/javascript">
+
+	  var _gaq = _gaq || [];
+	  _gaq.push(['_setAccount', 'UA-1189029-8']);
+	  _gaq.push(['_setDomainName','expresspack.com.br']);
+	  _gaq.push(['_trackPageview']);
+
+	  (function() {
+	    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+	    ga.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'stats.g.doubleclick.net/dc.js';
+	    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+	  })();
+
+	</script>
+  <!-- Facebook Pixel Code -->
+  <script>
+  !function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+  n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
+  n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
+  t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
+  document,'script','https://connect.facebook.net/en_US/fbevents.js');
+
+  fbq('init', '1068366543222116');
+  fbq('track', "PageView");</script>
+  <noscript><img height="1" width="1" style="display:none"
+  src="https://www.facebook.com/tr?id=1068366543222116&ev=PageView&noscript=1"
+  /></noscript>
+  <!-- End Facebook Pixel Code -->
+</head>
+
+<body>
+
+    <?php include'includes/menu.php';?>
+<!-- Carousel
+    ================================================== -->
+    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+      <!-- Indicators -->
+      <ol class="carousel-indicators">
+        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+        <li data-target="#myCarousel" data-slide-to="1"></li>
+        <li data-target="#myCarousel" data-slide-to="2"></li>
+      </ol>
+      <div class="carousel-inner" role="listbox">
+        <div class="item active">
+          <img class="first-slide" src="img/banner1.jpg" alt="First slide">
+          <div class="container">
+            <div class="carousel-caption">
+              <h1>SOLICITE AGORA!</h1>
+              <p>Note: If you're viewing this page via a <code>file://</code> URL, the "next" and "previous" Glyphicon buttons on the left and right might not load/display properly due to web browser security rules.</p>
+              <p><a class="btn btn-lg btn-primary" href="#" role="button">Sign up today</a></p>
+            </div>
+          </div>
+        </div>
+        <div class="item">
+          <img class="second-slide" src="img/banner2.jpg" alt="Second slide">
+          <div class="container">
+            <div class="carousel-caption">
+              <h1>Another example headline.</h1>
+              <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+              <p><a class="btn btn-lg btn-primary" href="#" role="button">Learn more</a></p>
+            </div>
+          </div>
+        </div>
+        <div class="item">
+          <img class="third-slide" src="img/banner3.jpg" alt="Third slide">
+          <div class="container">
+            <div class="carousel-caption">
+              <h1>One more for good measure.</h1>
+              <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+              <p><a class="btn btn-lg btn-primary" href="#" role="button">Browse gallery</a></p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+      </a>
+    </div><!-- /.carousel -->
+    <!-- Page Content -->
+    <div class="container">
+    	<a href="#0" class="cd-top">Top</a> <!-- cd-nugget-info -->
+    	<h1 class="chamada">Sua entrega na hora certa</h1>
+        <div class="row mvv">
+            <div class="col-lg-12">
+            	<div class="row">
+		        <div class="col-lg-4 central">
+		          <img class="img-circle" src="img/valores1.jpg" alt="Generic placeholder image" width="140" height="140">
+		          <h2>Equipe Uniformizada</h2>
+		          <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
+		        </div><!-- /.col-lg-4 -->
+		        <div class="col-lg-4 central">
+		          <img class="img-circle" src="img/valores2.jpg" alt="Generic placeholder image" width="140" height="140">
+		          <h2>Para sua empresa</h2>
+		          <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.</p>
+		        </div><!-- /.col-lg-4 -->
+		        <div class="col-lg-4 central">
+		          <img class="img-circle" src="img/valores3.jpg" alt="Generic placeholder image" width="140" height="140">
+		          <h2>Atendimento On-line</h2>
+		          <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+		        </div><!-- /.col-lg-4 -->
+		      </div><!-- /.row -->
+                
+        	</div>
+    	</div>
+    </div>
+    <div class="centralp centralp-sm">
+      <div class="container">
+          <h1 class="chamada">Por que escolher a Expresspack</h1>
+          <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+      </div>
+    </div>
+    <div class="container">
+    	<h1 class="chamada">Nossos Clientes</h1>
+    	<!--CLIENTES-->
+    	<div class="row">
+		    <div class="col-md-12">
+                <div id="Carousel" class="carousel slide carouselG">
+                 
+                <ol class="carousel-indicators">
+                    <li data-target="#Carousel" data-slide-to="0" class="active"></li>
+                    <li data-target="#Carousel" data-slide-to="1"></li>
+                    <li data-target="#Carousel" data-slide-to="2"></li>
+                </ol>
+                 
+                <!-- Carousel items -->
+                <div class="carousel-inner">
+                    
+                <div class="item active">
+                	<div class="row">
+                	  <div class="col-md-3"><a href="" class="thumbnail"><img src="img/clientes/fiat.png" alt="Image" style="max-width:100%;"></a></div>
+                	  <div class="col-md-3"><a href="#" class="thumbnail"><img src="img/clientes/itau.png" alt="Image" style="max-width:100%;"></a></div>
+                	  <div class="col-md-3"><a href="#" class="thumbnail"><img src="img/clientes/folha.jpg" alt="Image" style="max-width:100%;"></a></div>
+                	  <div class="col-md-3"><a href="#" class="thumbnail"><img src="img/clientes/unilever.jpg" alt="Image" style="max-width:100%;"></a></div>
+                	</div><!--.row-->
+                </div><!--.item-->
+                 
+                <div class="item">
+                	<div class="row">
+                		<div class="col-md-3"><a href="#" class="thumbnail"><img src="img/clientes/bayer.gif" alt="Image" style="max-width:100%;"></a></div>
+                		<div class="col-md-3"><a href="#" class="thumbnail"><img src="img/clientes/bk.jpg" alt="Image" style="max-width:100%;"></a></div>
+                		<div class="col-md-3"><a href="#" class="thumbnail"><img src="img/clientes/chili-beans.jpg" alt="Image" style="max-width:100%;"></a></div>
+                		<div class="col-md-3"><a href="#" class="thumbnail"><img src="img/clientes/samsunggg.jpg" alt="Image" style="max-width:100%;"></a></div>
+                	</div><!--.row-->
+                </div><!--.item-->
+                 
+                <div class="item">
+                	<div class="row">
+                		<div class="col-md-3"><a href="#" class="thumbnail"><img src="img/clientes/extra.jpg" alt="Image" style="max-width:100%;"></a></div>
+                		<div class="col-md-3"><a href="#" class="thumbnail"><img src="img/clientes/mrv.jpg" alt="Image" style="max-width:100%;"></a></div>
+                		<div class="col-md-3"><a href="#" class="thumbnail"><img src="img/clientes/leroy.jpg" alt="Image" style="max-width:100%;"></a></div>
+                		<div class="col-md-3"><a href="#" class="thumbnail"><img src="img/clientes/starbucks.gif" alt="Image" style="max-width:100%;"></a></div>
+                	</div><!--.row-->
+                </div><!--.item-->
+                 
+                </div><!--.carousel-inner-->
+                  <a data-slide="prev" href="#Carousel" class="left carousel-control ccontrol">‹</a>
+                  <a data-slide="next" href="#Carousel" class="right carousel-control ccontrol ">›</a>
+                </div><!--.Carousel-->
+        </div>
+		</div>
+
+
+    </div>
+    <!-- /.container -->
+    <div class="jumbotron jumbotron-sm">
+    	<div class="container">
+	        <div class="row">
+	            <div class="col-sm-12 col-lg-12">
+	                <h1 class="h1">
+	                    Contato <small>Entre em contato com a Expresspack Brooklin</small></h1>
+	            </div>
+	        </div>
+    	</div>
+    </div>
+	<div class="maps" id="contato">
+	<div class="container">
+	    <div class="row">
+	        <div class="col-md-8">
+	            <div class="well well-sm">
+	                <form>
+	                <div class="row">
+	                    <div class="col-md-6">
+	                        <div class="form-group">
+	                            <label for="name">
+	                                Nome</label>
+	                            <input type="text" class="form-control" id="name" placeholder="Seu nome" required="required" />
+	                        </div>
+	                        <div class="form-group">
+	                            <label for="email">
+	                                Endereço de E-mail</label>
+	                            <div class="input-group">
+	                                <span class="input-group-addon"><span class="glyphicon glyphicon-envelope"></span>
+	                                </span>
+	                                <input type="email" class="form-control" id="email" placeholder="Entre com um email" required="required" /></div>
+	                        </div>
+	                        <div class="form-group">
+	                            <label for="subject">
+	                                Assunto</label>
+	                            <select id="subject" name="subject" class="form-control" required="required">
+	                                <option value="na" selected="">Escolha um assunto:</option>
+	                                <option value="service">Financeiro</option>
+	                                <option value="suggestions">S.A.C</option>
+	                                <option value="product">Trabalhe conosco</option>
+	                            </select>
+	                        </div>
+	                    </div>
+	                    <div class="col-md-6">
+	                        <div class="form-group">
+	                            <label for="name">
+	                                Mensagem</label>
+	                            <textarea name="message" id="message" class="form-control" rows="9" cols="25" required="required"
+	                                placeholder="Mensagem"></textarea>
+	                        </div>
+	                    </div>
+	                    <div class="col-md-12">
+	                        <button type="submit" class="btn btn-success pull-right" id="btnContactUs">
+	                            Enviar Contato</button>
+	                    </div>
+	                </div>
+	                </form>
+	            </div>
+	        </div>
+	        <div class="col-md-4 detalhes">
+	            <form>
+	            <legend><span class="glyphicon glyphicon-globe"></span> Nosso Escritorio</legend>
+	            <address>
+                  <strong>ExpressPack, Brooklin.</strong><br>
+                  Rua Pensilvania, Brooklin, 1429<br>
+                  São Paulo, SP 01311-000<br>
+                  <abbr title="Phone">
+                      brooklin:</abbr>
+                  (11) 5505-3406 / (11) 5505-3407
+              </address>
+              <address>
+                  <strong>Contato Brooklin</strong><br>
+                  <a href="mailto:adm@expresspack.com.br">adm@expresspack.com.br</a>
+              </address>
+	            </form>
+	        </div>
+	    </div>
+	</div>
+	</div>
+  <div class="container">
+      <a href="#0" class="cd-top">Top</a> <!-- cd-nugget-info -->
+      <h1 class="chamada">Solicite ja sua entrega</h1>
+        <div class="row mvv">
+            <div class="col-lg-12">
+              <div class="row">
+            <div class="col-lg-4 central">
+              <img class="img-circle" src="img/telefone.jpg" alt="Ligue agora e solicite seu motoboy" width="140" height="140">
+              <h2>Telefones</h2>
+              <a href="tel:1155053406"><h3>(11) 5505-3406</h3></a><a href="tel:1155053407"><h3>(11) 5505-3407</h3></a>
+            </div><!-- /.col-lg-4 -->
+            <div class="col-lg-4 central">
+              <img class="img-circle" src="img/whatsapp.jpg" alt="Whatsapp seu contato rapido" width="140" height="140">
+              <h2>Whatsapp</h2>
+              <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.</p>
+            </div><!-- /.col-lg-4 -->
+            <div class="col-lg-4 central">
+              <img class="img-circle" src="img/email.jpg" alt="Solicite seu motoboy por email" width="140" height="140">
+              <h2>Atendimento On-line</h2>
+              <a href="mailto:adm@expresspack.com.br">adm@expresspack.com.br</a>
+            </div><!-- /.col-lg-4 -->
+          </div><!-- /.row -->
+                
+          </div>
+      </div>
+    </div>
+
+
+
+     <?php include'includes/footer.php';?>
+    <!-- child of the body tag -->
+	<script type="text/javascript">
+		$(document).ready(function() {
+		    $('#Carousel').carousel({
+		        interval: 5000
+		    })
+		});
+	</script>
+
+    <!-- jQuery -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/backtop.js"></script>
+    <script type="text/javascript">
+jQuery(document).ready(function($) { 
+    $(".scroll").click(function(event){        
+        event.preventDefault();
+        $('html,body').animate({scrollTop:$(this.hash).offset().top}, 800);
+   });
+});
+</script>
+</body>
+<!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/bootstrap-theme.min.css" rel="stylesheet">
+    <!-- Custom CSS -->
+    <link href="css/style.css" rel="stylesheet">
+    <link rel=”s"css/style.css" id="font-awesome-css" href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" type="text/css" media="screen">
+    <link href='https://fonts.googleapis.com/css?family=Lobster|Open+Sans' rel='stylesheet' type='text/css'>
+</html>
